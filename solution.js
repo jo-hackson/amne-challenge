bc6105e3cb735b3d13a87f5aa78d5848a3434dcd
Joanna <joanna.yeh.jackson@gmail.com>
2018-02-05 13:31:49 -0600

    Algo done

--- solution.js
+++ solution.js
@@ -33,18 +33,30 @@ var [averageHomeSalePrice] */
 
 function trendTracker(days, window, homePrices) {
 
-	let cache = {};
+	// let cache = null;
 
-	for (var j = 0; j < (days - window + 1); j++) {
+	for (let j = 0; j < (days - window + 1); j++) {
 		let testingWindow = homePrices.slice(j, window + j)
 		
 		let increased = 0;
 		let decreased = 0;
 
-		for (let i = 0; i < testingWindow.length - 1; i++) {
-			cache[something] = somethingelse;
-			if (testingWindow[i + 1] > testingWindow[i]) increased += 1;
-			if (testingWindow[i + 1] < testingWindow[i]) decreased += 1;
+		for (let i = 0; i < testingWindow.length; i++) {
+
+			// if (i == 0 && cache) {
+			// 	if (cache === "increased") increased += 1;
+			// 	if (cache === "decreased") decreased += 1;
+			// 	i++;
+			// }
+
+			if (testingWindow[i + 1] > testingWindow[i]) {
+				increased += 1;
+			}
+
+			if (testingWindow[i + 1] < testingWindow[i]) {
+				decreased += 1;
+			}
+
 			if (increased == testingWindow.length - 1) increased += 1
 			if (decreased == testingWindow.length - 1) decreased += 1
 		}
@@ -52,10 +64,14 @@ function trendTracker(days, window, homePrices) {
 	}
 }
 
+// console.time(trendTracker);
 trendTracker(5, 3, [188930, 194123, 201345, 154243, 154243]);
-
-// there is a way to make fast by caching
-// because right now, I am doing some extra comparisons
-// go through array and check at each interval and cache whether it is decreasing or increasing
+// trendTracker(10, 3, [188930, 194123, 201345, 154243, 154243, 188930, 194123, 201345, 154243, 154243]);
+// trendTracker(20, 3, [188930, 194123, 201345, 154243, 154243, 188930, 194123, 201345, 154243, 154243, 188930, 194123, 201345, 154243, 154243, 188930, 194123, 201345, 154243, 154243]);
+// console.timeEnd(trendTracker); 
+// 2.912ms without cache
+// 3.061ms with cache
+// 2.938ms without cache
+// 4.954ms with cache
 
 
